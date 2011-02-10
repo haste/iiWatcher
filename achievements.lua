5d3bddaa7d400e14106bac627f26241c28fad63f
Trond A Ekseth <troeks@gmail.com>
2011-02-10 18:16:54 +0100

    Remove the descriptions and replace gold/silver/copper icons with g/s/c.

--- achievements.lua
+++ achievements.lua
@@ -1,5 +1,12 @@
 -- Env. proxy. This way we most likely won't have to re-implement tons of stuff
 -- every patch something changes.
+
+local damnTextures = {
+	['Interface\\MoneyFrame\\UI-GoldIcon'] = 'g',
+	['Interface\\MoneyFrame\\UI-SilverIcon'] = 's',
+	['Interface\\MoneyFrame\\UI-CopperIcon'] = 'c',
+}
+
 local env = setmetatable({
 	GetAchievementInfo = function(...)
 		if(select('#', ...) == 1) then
@@ -15,12 +22,23 @@ local env = setmetatable({
 				end
 			end
 
+			if(numCriteria > 0) then
+				description = ''
+			end
+
 			if(numCompleted > 0) then
 				name = ('%s [%d/%d]'):format(name, numCompleted, numCriteria)
 			end
 
 			return id, name, points, completed, month, day, year, description, flags, icon, rewardText
 		end
+	end,
+
+	GetAchievementCriteriaInfo = function(...)
+		local description, type, completed, quantity, requiredQuantity, characterName, flags, assetID, quantityString, criteriaID, eligible = GetAchievementCriteriaInfo(...)
+		quantityString = quantityString:gsub('|T(.-):.-|t', damnTextures):gsub('/', ' / ')
+
+		return description, type, completed, quantity, requiredQuantity, characterName, flags, assetID, quantityString, criteriaID, eligible
 	end,
 }, {__index = _G})
 
